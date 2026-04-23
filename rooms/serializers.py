d7657348592b9b4ba4f97c03615fd733663c74ce
maratka09 <tomarat2009@gmail.com>
2026-04-23 18:55:59 +0600

    feat: Сделано / реализована логика входа по QR и API мест

--- rooms/serializers.py
+++ rooms/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import Place, Room
 
 class PlaceSerializer(serializers.ModelSerializer):
-    # Достаем имя через связь 'user', которую прописал твой коллега
+
     user_name = serializers.CharField(source='user.name', read_only=True, allow_null=True)
 
     class Meta:
@@ -10,7 +10,7 @@ class PlaceSerializer(serializers.ModelSerializer):
         fields = ['id', 'number', 'status', 'user_name']
 
 class RoomPlacesSerializer(serializers.ModelSerializer):
-    # 'places' — это тот самый related_name из models.py
+    
     places = PlaceSerializer(many=True, read_only=True) 
 
     class Meta:
